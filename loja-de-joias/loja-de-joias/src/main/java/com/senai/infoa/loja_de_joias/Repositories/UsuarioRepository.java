d87894f6ba8c84d3bfaeea564f43ad6611c58901
Maria Clara Sibernagel <mariaclaracostasibernagel@gmail.com>
2025-11-05 08:55:49 -0300

    Update UsuarioRepository.java

--- loja-de-joias/loja-de-joias/src/main/java/com/senai/infoa/loja_de_joias/Repositories/UsuarioRepository.java
+++ loja-de-joias/loja-de-joias/src/main/java/com/senai/infoa/loja_de_joias/Repositories/UsuarioRepository.java
@@ -3,7 +3,7 @@ package com.senai.infoa.loja_de_joias.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.senai.infoa.loja_de_joias.models.Usuario;
+import com.senai.infoa.loja_de_joias.Models.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
@@ -11,3 +11,4 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 }
 
     
+
